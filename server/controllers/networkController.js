4049f33ea2ba36b853e26e97676edac8a51cbc8a
subramanyam <subramanyam.kmk03@gmail.com>
2019-03-23 19:05:31 +0530

    update networkController.js

--- server/controllers/networkController.js
+++ server/controllers/networkController.js
@@ -5,25 +5,31 @@ const irisTesting = require('../datasets/iris_testing.json');
 // Mapping the training data 130 samples
 const trainingData = tf.tensor2d(irisTraining.map(item=> [
     item.sepal_length, item.sepal_width, item.petal_length, item.petal_width
-]), [130,4])
+]), [130,4]);
 
 const outputData = tf.tensor2d(irisTraining.map(item => [
     item.species === 'setosa' ? 1 : 0,
     item.species === 'virginica' ? 1 : 0,
     item.species === 'versicolor' ? 1 : 0
-]), [130,3])
+]), [130,3]);
 
 // Mapping the testing data 14 samples
 const testingData = tf.tensor2d(irisTesting.map(item=> [
     item.sepal_length, item.sepal_width, item.petal_length, item.petal_width
-]), [14,4])
+]), [14,4]);
+
+const outTestData = tf.tensor2d(irisTesting.map(item => [
+    item.species === 'setosa' ? 1 : 0,
+    item.species === 'virginica' ? 1 : 0,
+    item.species === 'versicolor' ? 1 : 0
+]), [14,3]);
 
 const buildNetwork = (layers, activ_func, regul_func, regul_rate, lr) => {
 	
 	const model = tf.sequential();
 
-	//Choosing regularizer
-	var regularizer = tf.regularizers.l1({
+	//Choosing regularizer function
+	let regularizer = tf.regularizers.l1({
 		l1: regul_rate
 	});
 	if(regul_func == 'l2') {
@@ -82,11 +88,12 @@ async function trainModel(trainingData, outputData, model) {
 function testModel(testingData, model) {
 	console.log('....Testing Output....');
 	model.predict(testingData).print();
+	outTestData.print();
 };
 
 async function runNetwork(req, res) {
-	const layers = [1, 2, 3, 4];
-	const activ_func = 'relu';
+	const layers = [10];
+	const activ_func = 'sigmoid';
 	const regul_func = 'l1';
 	const regul_rate = 0.01;
 	const lr = 0.01;
